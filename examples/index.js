ac29ed3e541101c0fc6abc47924af9f9f69fd704
Brian Mwakima <mwadime@fortunekidew.co.ke>
2018-06-19 21:48:24 +0300

    updates

--- examples/index.js
+++ examples/index.js
@@ -1,27 +1,14 @@
 const Paubox = require('../index');
 
-Paubox.setApiKey('24f5564f0265ad1120e72eb4d6c045a5');
-Paubox.setApiUsername('trylevel');
+Paubox.setApiKey('API KEY');
+Paubox.setApiUsername('username');
 Paubox.isTestMode(true);
 
 const data = {
-  to: 'Brian Mwadime <brian.mwadime@gmail.com>',
-  from: 'brian@trylevel.com',
-  subject: 'Integration Tests',
+  to: 'Brian Mwadime <test.test@test.com>',
+  from: 'test@test.com',
+  subject: 'Tests',
   text: 'Hello World!',
-  html: '<strong>Hello World!</strong>',
-  // message: {
-  //   recipients: [
-  //     'Brian Mwadime <brian.mwadime@gmail.com>'
-  //   ],
-  //   headers: {
-  //     from: 'brian@trylevel.com',
-  //     subject: 'Integration Tests',
-  //     'reply-to': 'brian@trylevel.com'
-  //   },
-  //   content: {
-  //     'text/plain': 'Hello World!'
-  //   }
-  // }
+  html: '<strong>Hello World!</strong>'
 };
 Paubox.send(data);
